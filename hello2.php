224bd93ba4ea8b231b73567fd600693b99c7579e
shreya <shreya1999wason@gmail.com>
2020-03-23 14:27:40 +0530

    hello2

---
+++ hello2.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+    <title> Form </title>
+</head>
+<body>
+  <form action="addRecord.php" method="post">
+      id - <input name="id" type="text" placeholder="Enter User Id"><br><br>
+      name - <input name="name" type="text" placeholder="Name"><br><br>
+      age - <input name="age" type="text" placeholder="Enter Age"><br><br>
+      contact - <input name="contact" type="number" placeholder="Enter Contact No."><br><br>
+      email - <input name="email" type="email" placeholder="Enter Email Id"><br><br>
+      <input name="submit" type="submit">
+
+  </form>
+
+</body>
+
+</html>
